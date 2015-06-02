d136bf814669783c3e421d5e5f69329f86f5851f
Dmitry Gridchin <gridchin.adi@gmail.com>
2015-06-02 22:22:43 +0300

    UI is created

---
+++ app/src/main/java/com/itcms/onoff_simplifiedui/controllers/MainActivity.java
@@ -0,0 +1,21 @@
+package com.itcms.onoff_simplifiedui.controllers;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.itcms.onoff_simplifiedui.R;
+
+/**
+ * Created by Дмитрий on 02.06.2015.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+    }
+
+}
