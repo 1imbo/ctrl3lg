e03dffe78ace1e5c97f7f28a2c5d7b72a43e3e79
Dmitry Gridchin <gridchin.adi@gmail.com>
2015-06-02 23:32:38 +0300

    switch to fullscreen

--- app/src/main/java/com/itcms/onoff_simplifiedui/controllers/MainActivity.java
+++ app/src/main/java/com/itcms/onoff_simplifiedui/controllers/MainActivity.java
@@ -1,8 +1,9 @@
 package com.itcms.onoff_simplifiedui.controllers;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.itcms.onoff_simplifiedui.R;
 
@@ -14,6 +15,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.main_activity);
     }
